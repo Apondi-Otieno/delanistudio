6f6bf1ce85b67999c4e2902528380bff8eecc6e8
Apondi Otieno <apondotii@gmail.com>
2022-01-03 20:24:37 +0300

    this will edit togle

--- script.js
+++ script.js
@@ -2,21 +2,16 @@
 
 // add jquery hover property
 
-$(document).ready(function() {
-    $("img").hover(function() {
-        
-    })
-})
 
 // add toggle for what we do section
 
-// $(function() {
-//     toggleWhatWeDo('.management-icon', '.management-text');
-//     toggleWhatWeDo('.management-text', '.management-icon');
-//     toggleWhatWeDo('.development-icon', '.development-text');
-//     toggleWhatWeDo('.development-text', '.development-icon');
-//     toggleWhatWeDo('.design-icon', '.design-text');
-//     toggleWhatWeDo('.design-text', '.design-icon');
+$(function() {
+    toggleWhatWeDo('.management-icon', '.management-text');
+    toggleWhatWeDo('.management-text', '.management-icon');
+    toggleWhatWeDo('.development-icon', '.development-text');
+    toggleWhatWeDo('.development-text', '.development-icon');
+    toggleWhatWeDo('.design-icon', '.design-text');
+    toggleWhatWeDo('.design-text', '.design-icon');
 
-// })
+})
 

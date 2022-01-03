bf2587e85525ce9d69e892617042df83463610a9
Apondi Otieno <apondotii@gmail.com>
2022-01-03 22:17:45 +0300

    this will add js

--- script.js
+++ script.js
@@ -1,40 +1,116 @@
 // add form alert and mailchimp
 
-let form = $('form');
-let name =$('#name');
-let email = $('#email');
-let message = $('#message');
 
-form.submit(function(event) {
-    event.preventDefault();
-    if(message.val().length ==0) {
-        alert('How can we help?')
-    }
-    else {
-        alert ('Hey $name.we have received your message')
-    }
-    form[0].reset();
-}
+
+
+
 // add jquery hover property
+
+
+
+
 
 
 // add toggle for what we do section
 
-$(function () {
-    toggleWhatWeDo('.management-icon', '.management-text');
-    toggleWhatWeDo('.management-text', '.management-icon');
-    toggleWhatWeDo('.development-icon', '.development-text');
-    toggleWhatWeDo('.development-text', '.development-icon');
-    toggleWhatWeDo('.design-icon', '.design-text');
-    toggleWhatWeDo('.design-text', '.design-icon');
-
-})
-
-function toggleWhatWeDo(class1, class2) 
-{
-    $(class1).click(function)
-    {
-        $(this).addClass('d-none');
-        $(class2).removeClass('d-none');
-    }
-}
+$(document).ready(function(){
+    $("#development-image").click(function(){
+      $("#development-image").slideDown('1500').hide('1000');
+      $("#development").show('1500');
+    });
+    $("#development").click(function(){
+      $("#development").slideUp('1500');
+      $("#development-image").slideDown('1500');
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#design-image").click(function(){
+      $("#design-image").slideDown('1500').hide('1000');
+      $("#design").show('1500');
+    });
+    $("#design").click(function(){
+      $("#design").slideUp('1500');
+      $("#design-image").slideDown('1500');
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#product-image").click(function(){
+      $("#product-image").slideDown('1500').hide('1000');
+      $("#product").show('1500');
+    });
+    $("#product").click(function(){
+      $("#product").slideUp('1500');
+      $("#product-image").slideDown('1500');
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#work1").mouseover(function(){
+      $("#overlay").show();
+    }).mouseout(function(){
+      $("#overlay").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work2").mouseover(function(){
+      $("#overlay2").show();
+    }).mouseout(function(){
+      $("#overlay2").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work3").mouseover(function(){
+      $("#overlay3").show();
+    }).mouseout(function(){
+      $("#overlay3").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work4").mouseover(function(){
+      $("#overlay4").show();
+    }).mouseout(function(){
+      $("#overlay4").hide();
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#work5").mouseover(function(){
+      $("#overlay5").show();
+    }).mouseout(function(){
+      $("#overlay5").hide();
+    });
+    $("#work6").mouseover(function(){
+      $("#overlay6").show();
+    }).mouseout(function(){
+      $("#overlay6").hide();
+    });
+    $("#work7").mouseover(function(){
+      $("#overlay7").show();
+    }).mouseout(function(){
+      $("#overlay7").hide();
+    });
+    $("#work8").mouseover(function(){
+      $("#overlay8").show();
+    }).mouseout(function(){
+      $("#overlay8").hide();
+    });
+  });
+  
+  $(document).ready(function(){
+    $("form#form34A").submit(function(event){
+      // event.preventDefault();
+      var name = $("input#MERGE1").val();
+      var email = $("input#MERGE0").val();
+      var message = $("textarea#message").val();
+      if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+        alert (name + ", we have received your message. Thank you for reaching out to us.");
+      }
+      else {
+        alert("Please enter your name and email!");
+      }
+      
+    });
+  
+  });
